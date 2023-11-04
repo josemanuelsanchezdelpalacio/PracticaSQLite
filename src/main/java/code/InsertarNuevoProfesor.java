0d14d77d7e4fece8ba6cd12a2d8ab76c6fd0a1d9
josemanuelsanchezdelpalacio <170068@iessanalberto.com>
2023-11-04 19:58:50 +0100

    añadida la clase ListarProfesores para listarlos segun su especialidad y la clase InsertarNuevoProfesor con validaciones

--- src/main/java/code/InsertarNuevoProfesor.java
+++ src/main/java/code/InsertarNuevoProfesor.java
@@ -6,105 +6,158 @@ import libs.Leer;
 import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
 
 public class InsertarNuevoProfesor {
 
     public static void insertarProf() {
-        Profesores nuevoProfesor = new Profesores();
 
+        int COD_PROF = 0, JEF_DEP = 0, COD_CENTRO = 0;
+        String NOMBRE_APE = "", ESPECIALIDAD = "", SEXO = "", FECHA_NAC = null;
+        PreparedStatement ps;
+        ResultSet rs;
+        boolean validacion;
         try (Connection conexion = ConexionSQLite.conectar()) {
-            if (validarCodigos(nuevoProfesor.getCOD_PROF(), conexion) && validarEspecialidad(String.valueOf(nuevoProfesor.getESPECIALIDAD()), conexion) && validarCodigos(nuevoProfesor.getESPECIALIDAD(), conexion) && validarFechaNacimiento(nuevoProfesor.getFECHA_NAC()) && validarSexo(String.valueOf(nuevoProfesor.getSEXO())) && validarCodigos(nuevoProfesor.getCOD_CENTRO(), conexion)) {
+            try (Statement consultaEspecialidad = conexion.createStatement()) {
 
-                nuevoProfesor.setCOD_PROF(Leer.pedirEntero("Introduce codigo de profesor: "));
-                nuevoProfesor.setJEFE_DEP(Leer.pedirEntero("Introduce codigo del jefe de departamento: "));
-                nuevoProfesor.setNOMBRE_APE(Leer.pedirCadena("Introduce nombre del profesor: "));
-                nuevoProfesor.setESPECIALIDAD(Leer.pedirChar("Introduce especialidad: "));
-                nuevoProfesor.setSEXO(Leer.pedirChar("Introduce sexo del profesor (H/M): "));
-                nuevoProfesor.setFECHA_NAC(Leer.pedirFecha("Introduce fecha de nacimiento (yyyy-MM-dd): ", "yyyy-MM-dd"));
-                nuevoProfesor.setCOD_PROF(Leer.pedirEntero("Introduce codigo del centro: "));
+                //comprobaciones para el codigo del profesor
+                do {
+                    validacion = true;
+                    COD_PROF = Leer.pedirEntero("Introduzca codigo de profesor: ");
+                    ps = conexion.prepareStatement("SELECT COD_PROF FROM C1_PROFESORES WHERE COD_PROF = ?;");
+                    ps.setInt(1, COD_PROF);
+                    rs = ps.executeQuery();
+                    if (rs.next()) {
+                        validacion = false;
+                        System.out.println("El codigo ya existe");
+                    }
+                    if ((COD_PROF + "").length() > 4) {
+                        validacion = false;
+                        System.out.println("El codigo no puede tener mas de 4 numeros");
+                    }
+                } while (!validacion);
 
-                PreparedStatement ps = conexion.prepareStatement("INSERT INTO C1_PROFESORES (COD_PROF, NOMBRE_APE, ESPECIALIDAD, JEFE_DEP, FECHA_NAC, SEXO, COD_CENTRO) VALUES (?, ?, ?, ?, ?, ?, ?)");
-                ps.setInt(1, nuevoProfesor.getCOD_PROF());
-                ps.setString(2, nuevoProfesor.getNOMBRE_APE());
-                ps.setString(3, String.valueOf(nuevoProfesor.getESPECIALIDAD()));
-                ps.setInt(4, nuevoProfesor.getJEFE_DEP());
-                ps.setString(5, String.valueOf(nuevoProfesor.getFECHA_NAC()));
-                ps.setString(6, String.valueOf(nuevoProfesor.getSEXO()));
-                ps.setInt(7, nuevoProfesor.getCOD_CENTRO());
+                //comprobaciones para la especialidad del profesor
+                ResultSet datosEspecialidad = consultaEspecialidad.executeQuery("SELECT * FROM C1_ESPECIALIDAD");
+                System.out.println("Lista de especialidades");
+                while (datosEspecialidad.next()) {
+                    System.out.print(datosEspecialidad.getString("ESPECIALIDAD") + " - ");
+                }
+                do {
+                    validacion = true;
+                    ESPECIALIDAD = Leer.pedirCadena("\nIntroduzca especialidad: ");
+                    ps = conexion.prepareStatement("SELECT ESPECIALIDAD FROM C1_ESPECIALIDAD WHERE ESPECIALIDAD = ?;");
+                    ps.setString(1, ESPECIALIDAD);
+                    rs = ps.executeQuery();
+
+                    if (!rs.next()) {
+                        validacion = false;
+                        System.out.println("Esa especialidad no existe");
+                    }
+
+                    if (ESPECIALIDAD.length() > 2) {
+                        validacion = false;
+                        System.out.println("Tiene que ser como maximo dos letras");
+                    }
+                } while (!validacion);
+
+                //comprobaciones nombre del profesor
+                do {
+                    validacion = true;
+                    NOMBRE_APE = Leer.pedirCadena("Introduzca nombre del profesor: ");
+                    if (NOMBRE_APE.length() > 30) {
+                        validacion = false;
+                        System.out.println("El nombre no puede tener mas de 30 letras");
+                    }
+                } while (!validacion);
+
+                //comprobaciones para el codigo del jefe de departamento
+                do {
+                    validacion = true;
+                    JEF_DEP = Leer.pedirEntero("Introduzca codigo del jefe de departamento: ");
+                    ps = conexion.prepareStatement("SELECT JEFE_DEP FROM C1_PROFESORES WHERE JEFE_DEP = ?;");
+                    ps.setInt(1, JEF_DEP);
+                    rs = ps.executeQuery();
+                    if (rs.next()) {
+                        validacion = false;
+                        System.out.println("El codigo ya existe");
+                    }
+                    if ((JEF_DEP + "").length() > 4) {
+                        validacion = false;
+                        System.out.println("El codigo tiene mas de 4 cifras");
+                    }
+                } while (!validacion);
+
+                //comprobaciones para la fecha de nacimiento
+                do {
+                    validacion = true;
+                    FECHA_NAC = Leer.pedirCadena("Introduzca fecha de nacimiento (yyyy-MM-dd): ");
+
+                    if (FECHA_NAC.length() != 10) {
+                        validacion = false;
+                        System.out.println("La fecha debe tener exactamente 10 caracteres (yyyy-MM-dd).");
+                    }
+
+                    // También puedes agregar una validación adicional para el formato de la fecha
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                    sdf.setLenient(false); // Esto hará que la fecha sea válida solo si cumple el formato
+
+                    try {
+                        sdf.parse(String.valueOf(FECHA_NAC));
+                    } catch (ParseException e) {
+                        validacion = false;
+                        System.out.println("El formato de la fecha es incorrecto.");
+                    }
+                } while (!validacion);
+
+                //comprobaciones del sexo del profesor
+                do {
+                    validacion = true;
+                    SEXO = Leer.pedirCadena("Introduzca sexo del profesor: ");
+                    if (!SEXO.equals("H") && !SEXO.equals("M")) {
+                        validacion = false;
+                        System.out.println("Tiene que ser H o M");
+                    }
+                    if (SEXO.length() > 1) {
+                        validacion = false;
+                        System.out.println("No puede ser mas largo que una letra (H-M)");
+                    }
+                } while (!validacion);
+
+                //comprobaciones para el codigo del profesor
+                do {
+                    validacion = true;
+                    COD_CENTRO = Leer.pedirEntero("Introduzca codigo del centro: ");
+                    ps = conexion.prepareStatement("SELECT COD_CENTRO FROM C1_PROFESORES WHERE COD_CENTRO = ?;");
+                    ps.setInt(1, COD_CENTRO);
+                    rs = ps.executeQuery();
+                    if (rs.next()) {
+                        validacion = false;
+                        System.out.println("El codigo ya existe");
+                    }
+                    if ((COD_CENTRO + "").length() > 4) {
+                        validacion = false;
+                        System.out.println("El codigo no puede tener mas de 4 numeros");
+                    }
+                } while (!validacion);
+
+                //insertamos los datos del profesor en la base de datos
+                ps = conexion.prepareStatement("INSERT INTO C1_PROFESORES VALUES (?,?,?,?,?,?,?)");
+                ps.setInt(1, COD_PROF);
+                ps.setString(2, NOMBRE_APE);
+                ps.setString(3, ESPECIALIDAD);
+                ps.setInt(4, JEF_DEP);
+                ps.setString(5, String.valueOf(FECHA_NAC));
+                ps.setString(6, SEXO);
+                ps.setInt(7, COD_CENTRO);
                 ps.execute();
-                System.out.println("Profesor insertado correctamente.");
-            } else {
-                System.out.println("ERROR. No se pudo insertar el profesor debido a errores de validacion.");
             }
         } catch (SQLException e) {
-            throw new RuntimeException("Error al conectar o insertar profesor: " + e.getMessage());
+            throw new RuntimeException(e);
         }
-    }
-
-    private static boolean validarCodigos(int codigo, Connection conexion) throws SQLException {
-        PreparedStatement ps = conexion.prepareStatement("SELECT COD_PROF FROM C1_PROFESORES WHERE COD_PROF = ?;");
-        ps.setInt(1, codigo);
-        ResultSet rs = ps.executeQuery();
-        if (rs.next()) {
-            System.out.println("El codigo ya existe: " + codigo);
-            return false;
-        }
-        if (String.valueOf(codigo).length() > 4) {
-            System.out.println("El codigo no puede tener más de 4 dígitos: " + codigo);
-            return false;
-        }
-        return true;
-    }
-
-    private static boolean validarEspecialidad(String especialidad, Connection conexion) throws SQLException {
-        PreparedStatement ps = conexion.prepareStatement("SELECT ESPECIALIDAD FROM C1_ESPECIALIDAD WHERE ESPECIALIDAD = ?;");
-        ps.setString(1, especialidad);
-        ResultSet rs = ps.executeQuery();
-
-        // listo las especialidades para mostrarlas por pantalla
-        ResultSet datosEspecialidad = ps.executeQuery("SELECT * FROM C1_ESPECIALIDAD");
-        while (datosEspecialidad.next()) {
-            System.out.println(datosEspecialidad.getString("ESPECIALIDAD"));
-        }
-
-        if (!rs.next()) {
-            System.out.println("La especialidad no existe: " + especialidad);
-            return false;
-        }
-        if (especialidad.length() > 2) {
-            System.out.println("La especialidad no puede tener mas de 2 caracteres: " + especialidad);
-            return false;
-        }
-        return true;
-    }
-
-    private static boolean validarFechaNacimiento(Date fechaNacimiento) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        String fecha = "";
-
-        try {
-            dateFormat.parse(String.valueOf(fechaNacimiento));
-            if (fecha.length() != 10) {
-                System.out.println("La longitud de la fecha debe ser de 10 caracteres: " + fecha);
-                return false;
-            }
-            return true;
-        } catch (ParseException e) {
-            System.out.println("La fecha de nacimiento no es válida: " + fechaNacimiento);
-            return false;
-        }
-    }
-
-    private static boolean validarSexo(String sexo) {
-        if (!sexo.equals("H") && !sexo.equals("M")) {
-            System.out.println("El sexo debe ser H o M: " + sexo);
-            return false;
-        }
-        if (sexo.length() > 1) {
-            System.out.println("El sexo no puede ser mas largo que una letra (H/M): " + sexo);
-            return false;
-        }
-        return true;
     }
 }
+
--- src/main/java/code/ListarProfesores.java
+++ src/main/java/code/ListarProfesores.java
@@ -5,11 +5,11 @@ import libs.Leer;
 import java.sql.*;
 import java.util.ArrayList;
 
+/**listar los profesores que imparten clase en una determinada especialidad que el usuario elegirá de la lista
+ * de especialidades posibles que se le mostrarán por pantalla**/
 public class ListarProfesores {
 
-    //listo los profesores que imparten clase en una determinada especialidad que el usuario elegirá de la lista de especialidades posibles que se le mostrarán por pantalla
-    public static void consultarProfesores(){
-
+    public static void consultarProfesores() {
         String especialidad = "";
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -18,34 +18,39 @@ public class ListarProfesores {
         try (Connection con = ConexionSQLite.conectar()) {
             try (Statement consultaEspecialidad = con.createStatement()) {
 
-                // listo las especialidades para mostrarlas por pantalla
+                //listo las especialidades para mostrarlas
                 ResultSet datosEspecialidad = consultaEspecialidad.executeQuery("SELECT * FROM C1_ESPECIALIDAD");
                 while (datosEspecialidad.next()) {
                     System.out.println(datosEspecialidad.getString("ESPECIALIDAD"));
                 }
 
-                do{
+                do {
                     validacion = true;
-                    //pido al usuario para que introduzca una especialidad para buscarla a traves de una consulta
-                    especialidad = Leer.pedirCadena("Elije una especialidad para mostrar al profesor con esa esp. :");
-                    //compruebo la especialidad usando las tablas C1_PROFESORES Y C1_ESPECIALIDAD buscando la especialidad por posicion
+                    //pido al usuario una especialidad para buscarla a traves de una consulta
+                    especialidad = Leer.pedirCadena("Elije una especialidad para mostrar a los profesores con esa esp. :");
+
+                    //comprueba la especialidad usando las tablas C1_PROFESORES Y C1_ESPECIALIDAD buscando la especialidad por posicion
                     ps = con.prepareStatement("SELECT * FROM C1_PROFESORES p, C1_ESPECIALIDAD e WHERE p.ESPECIALIDAD=e.ESPECIALIDAD AND e.ESPECIALIDAD=?;");
                     ps.setString(1, especialidad);
                     rs = ps.executeQuery();
-                    //muestro a los profesores con esa especialidad
+                    boolean especialidadEncontrada = false;
+
+                    // muestro los profesores con esa especialidad
                     while (rs.next()) {
+                        especialidadEncontrada = true;
                         System.out.println("Codigo profesor: " + rs.getInt("COD_PROF") + ", Nombre Profesor: " + rs.getString("NOMBRE_APE") + ", ESPECIALIDAD: " + rs.getString("ESPECIALIDAD"));
                     }
-                    //hago las comprobaciones si no existe y si lo que introduce el usuario no sea mayor a dos letras
-                    if (!rs.next()) {
+
+                    // hago las validaciones si no existe y si lo que introduce el usuario no sea mayor a dos letras
+                    if (!especialidadEncontrada) {
                         validacion = false;
                         System.out.println("Esa especialidad no existe");
                     }
                     if (especialidad.length() > 2) {
                         validacion = false;
-                        System.out.println("Tiene que ser como maximo dos letras");
+                        System.out.println("Tiene que ser como máximo dos letras");
                     }
-                }while (!validacion);
+                } while (!validacion);
 
             } catch (SQLSyntaxErrorException e) {
                 System.out.println("Error en la sintaxis de la sentencia SQL: " + e.getMessage());
